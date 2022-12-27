7298206212b2ad552cdf7512e7fbfa2d3ff2a91f
ct-dkkim <dkkim@everytalk.co.kr>
2022-12-27 15:57:34 +0900

    User ID 검색 오류 수정

--- callPages/phone/chkData_id.php
+++ callPages/phone/chkData_id.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=utf-8 ');
 
 include_once dirname(__FILE__) . "/../../lib/setConfig.php";
     //create the SQL query string
-    $user=$_POST[ user_id ];
+    $user=$_POST['user_id' ];
 
     $sQuery = "Select USER_ID from REGISTER where USER_ID='$user' ";
     $oResult = mysqli_query($db,$sQuery);
